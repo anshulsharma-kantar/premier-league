7f9b2ba8910bc594859b531c4044a94b15183e36
anshulsharma-kantar <anshul.sharma1@kantar.com>
2023-01-31 12:49:15 +0530

    login authentication

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="238836288153-56o3g7an4t5p0feojmcu8l0sebj8qa9b.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
