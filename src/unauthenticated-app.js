0e572dc0486025173e7c9405820fd3fff75763b1
anshulsharma-kantar <anshul.sharma1@kantar.com>
2023-02-08 00:00:34 +0530

    Main page changed

--- src/unauthenticated-app.js
+++ src/unauthenticated-app.js
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import { jsx } from '@emotion/react';
 
-// import { Logo } from './components/logo';
 import { useAuth } from 'context/auth-context';
 import { GoogleLogin } from '@react-oauth/google';
+import './styles/main.css'
 
 function UnauthenticatedApp() {
    const { login } = useAuth()
@@ -12,7 +11,7 @@ function UnauthenticatedApp() {
       const payload = {
          idtoken: token.credential
       }
-      login({data: payload})
+      login({ data: payload })
       // login('login')
    }
    return (
@@ -26,22 +25,35 @@ function UnauthenticatedApp() {
             height: '100vh',
          }}
       >
-         {/* <Logo width="80" height="80" /> */}
-         <h1>Premier League</h1>
          <div
             css={{
                display: 'grid',
-               gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+               gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
                gridGap: '0.75rem',
             }}>
-            <GoogleLogin
-               onSuccess={credentialResponse => {
-                  loginHandler(credentialResponse)
-               }}
-               onError={() => {
-                  console.log('Login Failed');
-               }}
-            />
+            <div className='login-wrapper'>
+               <div className="avatar">
+                  <img alt="Kantar Logo" src="images/Kantar-Group-Logo-PNG-White.png" />
+               </div>
+               <div className="avatar">
+                  <img alt="Premier League Logo" src="images/premier-league.png" />
+               </div>
+               <h4 css={{
+                  fontSize: 'clamp(1.44rem, calc(1.05rem + 1.95vw), 2.44rem)',
+                  fontWeight: '500',
+                  margin: '0 0 2rem'
+               }}>Premier League</h4>
+               <form className='login-form'>
+                  <GoogleLogin
+                     onSuccess={credentialResponse => {
+                        loginHandler(credentialResponse)
+                     }}
+                     onError={() => {
+                        console.log('Login Failed');
+                     }}
+                  />
+               </form>
+            </div>
          </div>
       </div>
    )
