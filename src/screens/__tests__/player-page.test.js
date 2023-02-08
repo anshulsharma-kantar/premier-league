560ccbda5a00adf957588d57e519b280cbab90c0
anshulsharma1 <anshul.sharma1@kantar.com>
2023-02-08 19:02:24 +0530

    removed excess code

--- src/screens/__tests__/player-page.test.js
+++ src/screens/__tests__/player-page.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable testing-library/no-debugging-utils */
 import App from "App";
 import { screen } from "@testing-library/react";
-import { act, loginAsUser, render } from "test/app-test-utils";
+import { loginAsUser, render } from "test/app-test-utils"; //add act
 
 async function renderPlayerPageScreen({ user }) {
     if (user === undefined) {
@@ -19,8 +19,3 @@ async function renderPlayerPageScreen({ user }) {
    expect(filterButton).toBeInTheDocument();
 
  })
-
- // a helper to use promises with timeouts
-function sleep(period) {
-   return new Promise(resolve => setTimeout(resolve, period));
-}
